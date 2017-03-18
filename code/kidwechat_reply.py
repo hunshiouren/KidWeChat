9d80cc64e654eb63a7e756f0831542ea24103dfe
draachen <draachen@163.com>
2017-03-18 23:03:38 +0800

    add kidwechat_reply.py

---
+++ code/kidwechat_reply.py
@@ -0,0 +1,40 @@
+#! python3
+# -*- coding:utf-8 -*-
+from wxpy import *
+
+bot = Bot(cache_path=True)
+help_info = """
+    帮助文档：
+    帮助，获取帮助文档
+    输入"寻找xx"，则寻找好友并创建群聊
+    输入"查找xx"，则查找公众号并返回二维码
+    输入其他，则开启自动聊天模式"""
+
+
+@bot.register(msg_types='Text')
+def reply(msg):
+    if msg.sender in bot.friends(): 
+        if msg.text[:2] == '寻找' and msg.text[2:]:
+            try:
+                friend_name = msg.text[2:]
+                search_friend = bot.friends().search(friend_name)[0]
+                users = [search_friend, msg.sender]
+                group = bot.create_group(users, topic='小白屋')
+                group.add_members(msg.sender, use_invitation=True)
+                group.add_members(search_friend, use_invitation=True)
+                group.send('Bingo!')
+            except:
+                return "{}还不是我的好友呢？".format(friend_name)
+            
+        elif msg.text[:2] == '查找' and msg.text[2:]:
+            return '查找公众号功能暂未上线'
+            
+        elif msg.text == '帮助':
+            return help_info
+        
+        else:
+            return '我听不懂你说什么呢？输入"帮助"试一下。'
+    else:
+        pass
+        
+bot.start()
