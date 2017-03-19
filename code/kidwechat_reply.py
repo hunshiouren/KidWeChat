9af12452a45936c4d55c58ac8b690bc3d3cc97c5
draachen <draachen@163.com>
2017-03-19 11:35:59 +0800

    use wxpy Tuling function

--- code/kidwechat_reply.py
+++ code/kidwechat_reply.py
@@ -1,16 +1,15 @@
 #! python3
 # -*- coding:utf-8 -*-
 from wxpy import *
-from autoreply import autoreply
 
 bot = Bot(cache_path=True)
 help_info = """
     帮助文档：
-    帮助，获取帮助文档
+    输入"帮助"，获取帮助文档
     输入"寻找xx"，则寻找好友并创建群聊
     输入"查找xx"，则查找公众号并返回二维码
     输入其他，则开启自动聊天模式"""
-
+tuling = Tuling(api_key='042c8822f369426aa4ff5396880c84ad')
 
 @bot.register(msg_types='Text')
 def reply(msg):
@@ -21,6 +20,7 @@ def reply(msg):
                 search_friend = bot.friends().search(friend_name)[0]
                 users = [search_friend, msg.sender]
                 group = bot.create_group(users, topic='小白屋')
+                msg.reply('{}已经找到，去"小白屋"里聊天吧。'.format(friend_name))
                 group.add_members(msg.sender, use_invitation=True)
                 group.add_members(search_friend, use_invitation=True)
                 group.send('Bingo!')
@@ -34,8 +34,8 @@ def reply(msg):
             return help_info
         
         else:
-            return autoreply(msg.text)
+            tuling.do_reply(msg)
     else:
         pass
         
-bot.start()
+bot.start(block=True)
