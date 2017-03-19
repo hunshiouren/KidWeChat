dc703ae285fbef6623c7695e4d5add9b03cfa532
draachen <draachen@163.com>
2017-03-19 21:45:22 +0800

    kidwechat_reply.py add 'add_friends'function

--- code/kidwechat_reply.py
+++ code/kidwechat_reply.py
@@ -9,6 +9,7 @@ HELP = """
     输入"帮助"，获取帮助文档
     输入"寻找xx"，则寻找好友并创建群聊
     输入"查找xx"，则查找公众号并返回二维码
+    输入"添加xx"，则向微信号'xx'发送好友请求
     输入其他，则开启自动聊天模式"""
 
 @bot.register(msg_types='Text')
@@ -24,14 +25,22 @@ def reply(msg):
             group.add_members(search_friend, use_invitation=True)
             group.send('Bingo!')
         except:
-            reply = "{}还不是我的好友呢？快把我推荐给他吧。".format(friend_name)
-            
+            reply = """
+                {}还不是我的好友呢？                推荐图图给他;或发送"添加xx",将他添加为图图的好友（仅支持微信号哦~）""".format(friend_name)
+
         msg.reply(reply)      
         
     elif msg.text[:2] == '查找' and msg.text[2:]:
         msg.reply('查找公众号功能暂未上线，请耐心等待哦。')
         
-    elif msg.text == '帮助':
+    elif msg.text[:2] == '添加' and msg.text[2:]:
+        user_id = msg.text[2:]
+        friend_name = msg.text[2:]
+        verify_info = '{}派我来的。'.format(msg.sender) 
+        bot.add_friend(user_id, verify_info)
+        msg.reply('对{}的好友请求已经发送，请稍后寻找他哦。'.format(friend_name))
+        
+    elif msg.text == '帮助' and not msg.text[2:]:
         msg.reply(HELP)
      
     else:
