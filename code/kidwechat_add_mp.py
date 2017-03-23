a892b088108cc63f2431f8f5289037ac6f7f74fc
李行之 <150349880@qq.com>
2017-03-23 21:54:43 +0800

    Update kidwechat_add_mp.py

--- code/kidwechat_add_mp.py
+++ code/kidwechat_add_mp.py
@@ -16,7 +16,7 @@ tuling = Tuling(api_key = KEY)
 dic_mp_name = {} #存储用户名称和公众号名称的字典
 dic_mp_qrcode = {} #存储公众号名称和二维码的字典
 
-mozart = '12 Mozart.jpg'  #暂时以莫扎特代替 24小时审核公众号图
+mozart = '12_Mozart.jpg'  #暂时以莫扎特代替 24小时审核公众号图
 
 #第一步: 用户发来公众号卡片，将用户名key：sender和value：公众号名称存储为字典
 @bot.register([Friend], msg_types = CARD)
@@ -30,7 +30,8 @@ def friend_send_mp(msg):
     msg.reply_image(mozart)
     msg.reply ('%s 发送了名片 %s' % (msg.chat.name, dic_mp_name[msg.chat.name]))
     #此时，需将mp，user,img写入数据库
-    add_data(dic_mp_name[msg.chat.name], msg.chat.name, dic_mp_qrcode[msg.card.name])
+    add_data(dic_mp_name[msg.chat.name], msg.chat.name)
+
 
 #第二步：用户查找公众号名称，返回图片
 @bot.register([Friend], msg_types = TEXT)
@@ -38,8 +39,8 @@ def friend_query_mp(msg):
     request_head = msg.text[:2]
     request_body = msg.text[2:]
     if request_head == '查找' and request_body:
-        #查找数据库，如果已添加此公众号，则返回图片
-        confirm_mp(request_body, msg.chat.name)
+        #查找数据库，如果已添加此公众号，则返回图片(默认返回莫扎特，手动添加二维码后返回二维码）)
+        search_img(request_body, msg.chat.name)
     else:
         tuling.do_reply(msg)
 
