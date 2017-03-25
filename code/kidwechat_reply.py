c643988730ba17be12ee577a7745acaba462fe2f
Ollie <lixinisme1115@126.com>
2017-03-25 20:13:47 +0800

    del start() and add embed()

--- code/kidwechat_reply.py
+++ code/kidwechat_reply.py
@@ -11,12 +11,12 @@ bot = Bot(cache_path=True)
 def reply(msg):
     reply = reply_text(msg)
     return reply
-        
+
 @bot.register([Group],TEXT)
 def reply(msg):
     if isinstance(msg.sender, Group) and msg.is_at:
         Tuling(api_key=KEY).do_reply(msg)
-        
+
 @bot.register(msg_types=FRIENDS)
 def auto_accept_friends(msg):
     new_friend = bot.accept_friend(msg.card)
@@ -28,4 +28,4 @@ def archive_mp(msg):
     return friend_send_mp(msg)
 
 
-bot.start(block=True)
+embed()
